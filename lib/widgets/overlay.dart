aebf38b7fb89a8c06ad49ac5a69a6b1b2cbf70fa
Deekshanta Paudel <deekshantapaudel@gmail.com>
2023-06-24 22:50:59 +0545

    improved overlay

--- lib/widgets/overlay.dart
+++ lib/widgets/overlay.dart
@@ -17,54 +17,56 @@ class OverlaySheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Add a Todo',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                color: Colors.white,
-                fontSize: 30,
+    return SingleChildScrollView( // Wrap with SingleChildScrollView
+      child: Padding(
+        padding:  EdgeInsets.only(top: 20, left: 20, right:20, bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Add a Todo',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            controller: titleController,
-            maxLength: 50,
-            keyboardType: TextInputType.name,
-            style: const TextStyle(color: Colors.white),
-            decoration: const InputDecoration(
-              labelText: 'Title',
-              labelStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 155, 171, 218),
+            const SizedBox(height: 10),
+            TextField(
+              controller: titleController,
+              maxLength: 50,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 155, 171, 218),
+                ),
+                counterStyle: TextStyle(color: Colors.white),
               ),
-               counterStyle: TextStyle(color: Colors.white),
             ),
-          ),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Cancel'),
-              ),
-              const Spacer(),
-              ElevatedButton(
-                onPressed: submitData,
-                child: const Text('Save Todo'),
-              ),
-            ],
-          ),
-        ],
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: submitData,
+                  child: const Text('Save Todo'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
